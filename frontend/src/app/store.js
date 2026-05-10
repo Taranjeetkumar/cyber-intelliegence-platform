2d34842f8c60d6110eb8462965f94e68cb36ef88
jeevitha <jeevitharamesh0123.com>
2026-05-10 17:36:48 +0200

    Campaign Alert page with API Integration

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import investigationReducer from "../features/investigation/investigationSlice";
 import threatSearchReducer from "../features/threatSearch/threatSearchSlice";
 import deviceRiskReducer from "../features/deviceRisk/DeviceRiskPage";
-
+import campaignAlertReducer from "../features/campaignAlert/CampaignAlertPage";
 export const store = configureStore({
   reducer: {
     investigation: investigationReducer,
     threatSearch: threatSearchReducer,
     deviceRisk: deviceRiskReducer,
+    campaignAlert: campaignAlertReducer,
   },
 });
 
